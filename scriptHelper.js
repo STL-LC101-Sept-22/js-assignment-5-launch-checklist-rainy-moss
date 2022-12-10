36f40b9e54bc14d42504ebc19b14192d0b0bedac
Rainy <112306653+rainy-moss@users.noreply.github.com>
2022-12-09 23:26:47 -0600

    fixed typos

--- scriptHelper.js
+++ scriptHelper.js
@@ -18,7 +18,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
 function validateInput(testInput) {
     testInput = Number(testInput);
-    if (testInput === ""){
+    if (testInput === "" || testInput === 0){
         return "Empty";
 
     }else if (isNaN(testInput) === false){
@@ -45,19 +45,19 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
 
     }else {
         list.style.visibility = "visible";
-        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch'`;
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilotStatus} is ready for launch`;
         launchStatus.innerHTML = "Shuttle ready for launch";
         launchStatus.style.color = "rgb(65, 159, 106)";
 
         if (fuelLevel < 10000 ){
         fuelStatus.innerHTML = "Fuel level too low for launch";
-        launchStatus.innerHTML = "Shuttle not ready for launch";
+        launchStatus.innerHTML = "Shuttle Not ready for launch";
         launchStatus.style.color = "rgb(199, 37, 78)";
 
         }if (cargoMass > 10000){
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-        launchStatus.innerHTML = "Shuttle not ready for launch";
+        launchStatus.innerHTML = "Shuttle Not ready for launch";
         launchStatus.style.color = "rgb(199, 37, 78)";
     }
     }
@@ -66,11 +66,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
 async function myFetch() {
     let planetsReturned;
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-        if (response.status >= 400){
-            throw new Error ("Bad response");
-        }else{
         return response.json()
-        }
     });
     return planetsReturned;
 }
